fd03a6a24f02162846c3109727007c3c7c5102f6
AmalieBoegild <abso@itu.dk>
2021-11-23 09:21:35 +0100

    Start server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,14 +2,39 @@ package server
 
 import(
 	pb "github.com/ap/DMP3/api"	
+	"log"
+	"net"
+	"google.golang.org/grpc"
+)
+
+const (
+	ip = "127.0.0.1:5001"
 )
 
 type Node struct{
 	HighestBid int32
+	pb.UnimplementedAuctionServer
 }
 
-func Main(){
+func main(){
+	node := &Node{
+		HighestBid: 0,
+	}
 
+	node.StartServer()
+}
+
+func(n *Node) StartServer(){
+	lis, err := net.Listen("tcp", ip)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterAuctionServer(s, n)
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
 
 func(n *Node) UpdateBid(newBid int32) pb.BidReply_Outcome{
